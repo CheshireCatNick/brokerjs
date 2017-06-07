c2e922ab5f3b374caecad2fcdc051951bb6c60f7
cheshirecatnick <cheshirecatnick@gmail.com>
2017-06-07 23:10:42 +0800

    add data

--- market.js
+++ market.js
@@ -1,9 +1,14 @@
-'use strick';
-
+'use strict';
+const bitcoinRate = require('./data/bitcoinRate');
 const Broker = require('./broker/NBroker');
 
 const broker = new Broker(1000);
 console.log(broker.asset);
+
+console.log(bitcoinRate);
+
+
+
 
 
 
